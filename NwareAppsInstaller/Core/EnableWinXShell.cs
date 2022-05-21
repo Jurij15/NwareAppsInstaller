dbfb01d4cf3a91d0b26c801411f77b9af1904c90
Jurij15 <jurij.grom1@gmail.com>
2022-05-21 19:26:07 +0200

    add auto win x shell

---
+++ NwareAppsInstaller/Core/EnableWinXShell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace NwareAppsInstaller.Core
+{
+    public class EnableWinXShell
+    {
+        public void WinXShell()
+        {
+            Console.WriteLine("Do you want to start WinXShell? [Y][N]");
+            string input = Console.ReadLine();
+            if (input == "y")
+            {
+                Console.WriteLine("Starting WinXShell");
+                Process p = new Process();
+                p.StartInfo.FileName = strings.WinXShellFInalExe;
+                p.Start();
+            }
+            else if (input == "n")
+            {
+                //do nothing
+            }
+        }
+    }
+}
