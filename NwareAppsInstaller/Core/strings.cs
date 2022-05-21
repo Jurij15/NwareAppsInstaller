eced6295cbc311b19dee9dee0ab3357d7581f8af
Jurij15 <jurij.grom1@gmail.com>
2022-05-21 16:38:31 +0200

    Add project files.

---
+++ NwareAppsInstaller/Core/strings.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NwareAppsInstaller.Core
+{
+    public static class strings
+    {
+    }
+}
---
+++ NwareAppsInstaller/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NwareAppsInstaller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NwareAppsInstaller, version 1.0, made by Jurij15 #1867");
+            Console.WriteLine("Initializing...");
+            Thread.Sleep(150);
+            Console.WriteLine("Downloads starting...");
+            //start downloading files
+            
+        }
+    }
+}
